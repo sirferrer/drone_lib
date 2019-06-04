ab6674bd67312c53a992187215e2254ac6df4b60
pablo <pafe2011@gmail.com>
2019-06-04 22:04:43 +0100

    created base missions take_off and land

--- src/headers/servo.h
+++
@@ -1,7 +0,0 @@
-#include <ros/ros.h>
-#include <mavros_msgs/OverrideRCIn.h>
-#include <mavros_msgs/Altitude.h>
-#include <mavros_msgs/SetMode.h>
-
-mavros_msgs::Altitude altitude;
-mavros_msgs::OverrideRCIn rc_command;
